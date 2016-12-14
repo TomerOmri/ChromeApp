b136b22466a926197ecee53650169dde8d3c23d1
polar_bear <tomer92@gmail.com>
2016-12-14 20:31:17 +0200

    add

--- home.js
+++ home.js
@@ -7,20 +7,23 @@ $(document).ready(function(){
     $("#showTime").append(str);
 
 
-$("#leftBar").animate({
+   // $('p span').css('color' ,'red');
 
 
-});;
+    $('ul#list li:first').css('color' ,'red');
+
+
+$("#nameOfUser").dblclick(function(){
+    var newName = prompt("Please enter your name");
+    $("#nameOfUser").empty().append(newName);
+});
 
 });
 
-$("#nameOfUser").onclick(function(){
-
-})
 
 
 function highLightButton(objectThatWasPressed) {
-    console.log(objectThatWasPressed);
+
     objectThatWasPressed.style.backgroundColor = "black";
 
 
@@ -28,7 +31,30 @@ function highLightButton(objectThatWasPressed) {
 
 function NOhighLightButton(objectThatWasPressed) {
 
-    console.log(objectThatWasPressed);
     objectThatWasPressed.style.backgroundColor = "rgba(0,0,0,0.2)";
 }
 
+$(".addTile").click(function(){
+   var nameOfTile = prompt("Enter name of tile:");
+   var linkToSite = prompt("Enter site address:");
+    var fullLinkPath = 'http://' + linkToSite;
+
+console.log(fullLinkPath);
+
+    var aLink = $('<a />').attr('href', fullLinkPath)
+        .attr('target', '_blank')
+        .text(nameOfTile);
+
+   var closeSpan = $('<span />').text(aLink)
+       .attr('id', 'menuButton')
+       .attr('onemouseover', 'highLightButton(this)')
+       .attr('onmouseleave', 'NOhighLightButton(this)');
+
+   console.log(closeSpan);
+
+
+
+
+   $('#menu').append(aLink);
+
+});
